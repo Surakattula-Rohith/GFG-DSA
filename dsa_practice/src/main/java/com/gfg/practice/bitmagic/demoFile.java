443c2e4372367dc5df4c7d9bf49805fb8d58acc8
srohith.charan <srohith.charan@gmail.com>
2024-01-21 13:03:32 +0530

    Cleaned the project.

--- dsa_practice/src/main/java/com/gfg/practice/bitmagic/demoFile.java
+++ dsa_practice/src/main/java/com/gfg/practice/bitmagic/demoFile.java
@@ -2,7 +2,6 @@ package com.gfg.practice.bitmagic;
 
 public class demoFile {
     public static void main(String[] args) {
-        System.out.println("Made changes by satyam");
-        System.out.println("ok bye");
+        System.out.println("Cleaned project.");
     }
 }
