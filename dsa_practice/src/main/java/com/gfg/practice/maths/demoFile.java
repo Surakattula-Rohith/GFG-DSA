d09f26d89dbe94c7a578529cd97a1b75978dfea7
srohith.charan <srohith.charan@gmail.com>
2024-01-21 11:50:22 +0530

    package for maths-code created.

---
+++ dsa_practice/src/main/java/com/gfg/practice/maths/demoFile.java
@@ -0,0 +1,12 @@
+package com.gfg.practice.maths;
+
+public class demoFile {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("This is demo class");
+        }
+    }
+}
